b950ccf60473eb45571c3c8a5b1e9b5f421cfc87
MartinsCode <Martinscode33@gmail.com>
2024-11-26 15:42:20 +0000

    updated style for shopping(shopping.css)

--- src/pages/shopping/Product.jsx
+++ src/pages/shopping/Product.jsx
@@ -5,13 +5,12 @@ const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
   return (
     <div className="product">
-      <img src={productImage} alt="" />
-      <div className="discription">
-        <b>
-          <p>{productName}</p>
-        </b>
-        <p>${price}</p>
+      <img src={productImage} alt={productName} />
+      <div className="description">
+        <p className="product-name">{productName}</p>
+        <p className="product-price">${price}</p>
       </div>
+      <button className="addToCartBttn">Add to Cart</button>
     </div>
   );
 };
